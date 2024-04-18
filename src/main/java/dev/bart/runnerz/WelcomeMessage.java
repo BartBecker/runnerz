bf5818def1e5862b9f9db73485a0e7360f68afac
Bart Becker <Bart Becker@DOCKWERKEN>
2024-04-18 11:32:46 +0200

    first commit

---
+++ src/main/java/dev/bart/runnerz/WelcomeMessage.java
@@ -0,0 +1,12 @@
+package dev.bart.runnerz;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeMessage {
+
+    public String getWelcomeMessage() {
+        return "Welcome to the Spring Boot Application!";
+
+    }
+}
